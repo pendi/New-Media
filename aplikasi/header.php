8b7de33ec8365b7d5f65273ad74da3740b65288d
pendi setiawan <sacktiawan@gmail.com>
2014-02-11 18:00:56 +0700

    pertama

---
+++ aplikasi/header.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Anila Shop</title>
+	<link rel="stylesheet" type="text/css" href="../css/global.css">
+	<link rel="shortcut icon" href="gambar/favicon_1.ico" />
+</head>
+<body bgcolor="#80B2FF">
+<table width="70%" align="center" bgcolor="#3385FF">
+	<tr>
+		<td colspan="2"><font color="white" size="30">&nbsp;ANILA SHOP</font></td>
+		<td align="right" style="vertical-align: top;">
+			<a href="login.php" style="text-decoration: none;">
+			<font color="white"> Login &nbsp;</font></a>
+		</td>
+	</tr>
+	<tr>
+		<td width="10%" align="center"><img src="http://localhost/new_media/aplikasi/gambar/huruf-a.png" height="30%"></td>
+		<td width="25%">Jual Laptop Baru Bergaransi</td>
+		<td width="35%" align="right"><font size="6">PENDI SETIAWAN</font></td>
+	</tr>
+</table>
+<table width="70%" align="center" bgcolor="#0052CC">
+	<tr>
+		<td colspan="2">
+			<ul class="dropmenu">
+				<li><a href="#1">Acer</a>
+					<ul>
+						<li><a href="#11">E1-421-11202G32Mn</a></li>
+						<li><a href="#12">E1-421-E302G32Mn</a></li>
+						<li><a href="#12">E1-422-12502G50Mn</a></li>
+						<li><a href="#12">E1-422-65202G50Mn</a></li>
+						<li><a href="#12">E1-431-10002G32Mn</a></li>
+						<li><a href="#12">E1-431-B832G32Mn</a></li>
+					</ul>
+				</li>
+				<li><a href="#2">Asus</a>
+					<ul>
+						<li><a href="#21">1225B</a></li>
+						<li><a href="#22">1225C</a></li>
+						<li><a href="#23">A43E-VX1070D</a></li>
+						<li><a href="#24">A43E-VX1071D</a></li>
+					</ul>
+				</li>
+				<li><a href="#3">Apple</a>
+					<ul>
+						<li><a href="#31">Sub Menu 1</a></li>
+						<li><a href="#32">Sub Menu 2</a></li>
+						<li><a href="#33">Sub Menu 3</a></li>
+					</ul>
+				</li>
+				<li><a href="#4">Dell</a>
+					<ul>
+						<li><a href="#41">Sub Menu 1</a></li>
+						<li><a href="#42">Sub Menu 2</a></li>
+						<li><a href="#43">Sub Menu 3</a></li>
+					</ul>
+				</li>
+				<li><a href="#4">Hp</a>
+					<ul>
+						<li><a href="#41">Sub Menu 1</a></li>
+						<li><a href="#42">Sub Menu 2</a></li>
+						<li><a href="#43">Sub Menu 3</a></li>
+					</ul>
+				</li>
+				<li><a href="#4">Lenovo</a>
+					<ul>
+						<li><a href="#41">Sub Menu 1</a></li>
+						<li><a href="#42">Sub Menu 2</a></li>
+						<li><a href="#43">Sub Menu 3</a></li>
+					</ul>
+				</li>
+				<li><a href="#4">Samsung</a>
+					<ul>
+						<li><a href="#41">Sub Menu 1</a></li>
+						<li><a href="#42">Sub Menu 2</a></li>
+						<li><a href="#43">Sub Menu 3</a></li>
+					</ul>
+				</li>
+				<li><a href="#4">Toshiba</a>
+					<ul>
+						<li><a href="#41">Sub Menu 1</a></li>
+						<li><a href="#42">Sub Menu 2</a></li>
+						<li><a href="#43">Sub Menu 3</a></li>
+					</ul>
+				</li>
+			</ul>
+		</td>
+	</tr>
+</table>
+<table width="70%" align="center" bgcolor="#E6E6E6">
+	<tr>
+		<td align="right" colspan="2">
+			<form action="search.php" method="post">
+				<input required type="search" name="search" placeholder="search">
+				<input type="submit" name="submit" value="search">		
+			</form>
+		</td>
+	</tr>
+</table>
+</body>
+</html>
