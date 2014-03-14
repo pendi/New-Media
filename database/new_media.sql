b544a21c6940dfb3a505aa52020d4e58cd011be2
Pendi Setiawan <sacktiawan@gmail.com>
2014-03-14 18:01:00 +0700

    14/03/2014

--- database/new_media.sql
+++ database/new_media.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 04, 2014 at 04:27 PM
--- Server version: 5.5.35-0ubuntu0.12.04.1
--- PHP Version: 5.3.10-1ubuntu3.9
+-- Generation Time: Mar 14, 2014 at 06:00 PM
+-- Server version: 5.5.35-0ubuntu0.12.04.2
+-- PHP Version: 5.3.10-1ubuntu3.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -56,13 +56,33 @@ CREATE TABLE IF NOT EXISTS `login` (
   `username` varchar(30) NOT NULL,
   `password` varchar(30) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
 -- Dumping data for table `login`
 --
 
 INSERT INTO `login` (`id`, `username`, `password`) VALUES
+(1, 'pendi', 'password');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login1`
+--
+
+CREATE TABLE IF NOT EXISTS `login1` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+
+--
+-- Dumping data for table `login1`
+--
+
+INSERT INTO `login1` (`id`, `username`, `password`) VALUES
 (1, 'pendi', '05-09-10'),
 (2, 'nida', 'nidapendi'),
 (3, 'reza', '1234'),
@@ -120,20 +140,25 @@ CREATE TABLE IF NOT EXISTS `product` (
 --
 
 INSERT INTO `product` (`id_product`, `name`, `type`, `price`, `description`, `stock`, `image`) VALUES
-('AC001', 'Acer', '', 2000000, '', 4, ''),
-('AC002', 'Acer', '', 1000000, '', 10, ''),
+('AC001', 'Acer', '', 2000000, '', 4, '../aplikasi/image/C360_2014-02-15-14-33-29-348.jpg'),
+('AC002', 'Acer33', 'klllllllllll', 1000000, 'vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv\r\nvvvvvvvvvvvvvvvvvv', 10, '../aplikasi/image/images (1).jpg'),
 ('AC003', 'Notebook Acer', '', 1500000, '', 5, ''),
 ('AC004', 'Acer', '', 2300000, '', 7, ''),
-('AC006', 'mjgv', 'Bekas', 2000000, 'Ancur\r\n', 3, 'gambar/994076_614082505323678_586749833_n.jpg'),
-('AS001', 'Asus', '', 1200000, '', 2, ''),
+('AC006', 'mjgv', 'Bekas', 2000000, 'Ancur\r\n', 6, '../aplikasi/image/C360_2014-02-15-14-21-48-834.jpg'),
+('AS001', 'Asus', '', 1200000, '', 2, '../aplikasi/image/images (1).jpg'),
 ('AS002', 'Asus', '', 1400000, '', 0, ''),
 ('AS003', 'Asus', 'Terbaru', 4100000, '', 5, ''),
-('bjmbg', 'jgfvjgv', 'hjgvhj', 0, 'bkjbhkhb', 0, 'gambar/sub6.jpg'),
-('gghgb', 'gvhgv', 'gvgv', 0, 'vbjgvjgbj', 0, 'gambar/Rizky Dana Saputro_108091000006.jpg'),
+('gghgb', 'gvhgv', 'gvgv', 0, 'vbjgvjgbj', 4, 'image/Rizky Dana Saputro_108091000006.jpg'),
 ('HP001', 'Hp', '', 2500000, '', 0, ''),
 ('HP002', 'Hp', '123', 3200000, '', 3, ''),
-('HP003', 'Hp', 'Sdim', 4400000, 'n ,kznjnjbjbfhhjbJ', 2, 'gambar/acer.jpg'),
-('uygby', 'hygvu', 'vy', 6565, 'hfcvgfvh', 4, 'gambar/sub7.jpg');
+('HP003', 'Hp', 'Sdim', 4400000, 'n ,kznjnjbjbfhhjbJ', 2, 'image/acer.jpg'),
+('jjjjj', 'jjjjjj', 'jjjjj', 0, 'jjjjjjj\r\njjjj\r\njjjjjjjjjjjjjj\r\njjjjjjjj\r\njjjjjjjjjjjjj', 0, '../aplikasi/image/images (2).jpg'),
+('lllll', 'lllllll', 'llllllll', 0, 'llllllllllllllllll\r\nllllllllllll\r\nllllllllllllllllllllllllllllllllllllllllllllllllllll\r\nlllllllllllllll\r\nllllllllllllllllll\r\nllllllll\r\nllllllllllllllllllllll\r\nllllllllllllllll\r\nll\r\nllllllllll', 45, '../aplikasi/image/images (1).jpg'),
+('mmmmm', 'mmmmm', 'mmmmmm', 0, 'mmmmmmmmm', 9, '../aplikasi/image/images.jpg'),
+('nnnnn', 'nnnnnnn', 'nnnnn', 0, '', 0, 'image/'),
+('uygby', 'hygvu', 'vy', 6565, 'hfcvgfvh', 4, 'image/sub7.jpg'),
+('vv', 'vvvv', 'vvvvv', 0, 'vvvvvvvvvvvv', 0, '../aplikasi/image/images (1).jpg'),
+('vvvv', 'vvvvvv', 'vvvvvv', 0, 'vvvvvvvv\r\nvvvvvvvvv\r\nvvv\r\nvvvvvv\r\nvvvvv', 0, '');
 
 -- --------------------------------------------------------
 
