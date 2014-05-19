2f5cc4f9c7b7de69c4664f5a0644cc4fbe145124
Pendi Setiawan <sacktiawan@gmail.com>
2014-05-19 15:12:27 +0700

    19/05/2014

--- admin/edit_user.php
+++ admin/edit_user.php
@@ -17,7 +17,7 @@ $data = mysql_fetch_array($query);
 		</tr>
 		<tr>
 			<td width="23%"></td>
-			<td width="12%">Change Usernam &nbsp;</td>
+			<td width="12%">Change Username &nbsp;</td>
 			<td width="35%"><input autofocus type="text" name="username" placeholder="Username" value="<?php echo $data[1]; ?>"></td>
 		</tr>
 		<tr>
