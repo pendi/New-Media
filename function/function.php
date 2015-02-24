9018ffe093267f5cfee33ac129971b7acbeae3e3
pendi <sacktiawan@gmail.com>
2015-02-24 13:45:52 +0700

    Add Function Auto Delete

--- function/function.php
+++ function/function.php
@@ -2,4 +2,11 @@
 	function price($price) {
 		$format = number_format($price,0,'','.').",-";
 		return $format;
-}
+	}
+
+	function autoDelete($table) {
+		$time = 3;
+		$query = "DELETE FROM $table WHERE DATEDIFF(CURDATE(), created_time) > $time";
+		$hasil = mysql_query($query);
+		return $hasil;
+	}
